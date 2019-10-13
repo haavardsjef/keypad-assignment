6eb83982f13094a342fb6c0477dc33361deb6deb
Håvard <havhje@gmail.com>
2019-10-13 13:15:21 +0200

    Finished Rule class

--- ikke_kok/rule.py
+++ ikke_kok/rule.py
@@ -2,22 +2,31 @@
 
 
 class Rule:
-    """Rules are made here"""
+    """Rules class"""
 
     def __init__(self, state1, state2, signal, action):
-        self.set_state_1(state1)
-        self.set_state_2(state2)
+        self.set_state1(state1)
+        self.set_state2(state2)
         self.set_signal(signal)
         self.set_action(action)
 
-    def set_state_1(self, state_1):
-        self.state_1 = state_1
+    def set_state1(self, state1):
+        """ Sets state1 """
+        self.state1 = state1
 
-    def set_state_2(self, state_2):
-        self.state_2 = state_2
+    def set_state2(self, state2):
+        """ Sets set_state2 """
+        self.state2 = state2
 
     def set_signal(self, signal):
+        """ Sets signal """
         self.signal = signal
 
     def set_action(self, action):
+        """ Sets action """
         self.action = action
+
+    def __str__(self):
+        """ Tostring method """
+        return "Rule: " + (self.state1) + " => " + str(self.state2) \
+        + ", " + str(self.signal) + ", " + str(self.action)
